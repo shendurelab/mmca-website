2a6bb23a73f7163ef02f8a09c6e995e212b5f07e
cooperqmarshall <qballer@uw.edu>
2022-04-22 16:02:03 -0700

    Updating mutant labels

--- public/js/mutant_map.js
+++ public/js/mutant_map.js
@@ -2,7 +2,8 @@ const mutant_map = {
     "Scn10a_Scn11a KO": "Scn10a/11a DKO",
     "Atp6v0a2RQ KI": "Atp6v0a2 R755Q",
     "Ttc21b MT": "Ttc21b KO",
-    "Cdkl5 KO": "Cdkl5 Y/-",
+    "Cdkl5 KO": "Cdkl5 -/Y",
+    "SScn11A GoF": "Scn11A GoF",
     "Zfp42 KO": "Fat1 TAD KO",
     "TAD boundary KI ": "Sox9 TAD boundary KI",
     "Sox9 Regulatory KO ": "Sox9 regulatory INV"
